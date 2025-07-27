83a72ad4c55fc1a340016b9b22f1ccc33d1b6618
furious-error <abhishek08092004@gmail.com>
2025-07-27 14:49:05 +0530

    route fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/ai-quiz-generator'>
       <AuthProvider>
         <div className="flex flex-col min-h-screen font-inter">
           <Navbar />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/ai-quiz-generator'>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
